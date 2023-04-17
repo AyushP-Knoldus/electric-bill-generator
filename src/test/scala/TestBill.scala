099e1ff3f339179d95cbec8fef5c61e7819b9d52
Ayush Pathak <125042596+AyushP-Knoldus@users.noreply.github.com>
2023-04-17 11:47:34 +0530

    Update TestBill.scala

--- src/test/scala/TestBill.scala
+++ src/test/scala/TestBill.scala
@@ -1,17 +1,11 @@
 package com.knoldus
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
 
-class TestBill extends AnyFlatSpec with Matchers {
-  //"A Bill" should "calculate the bill correctly" in {
-  try {
+class TestBill extends AnyFlatSpec{
+  "A Bill" should "calculate the bill correctly" in {
     val bill = Bill("1001", "user1", "Delhi", 5062, 5904)
     val expectedBillAmount = 7073.51
     assert(bill.calculateBill() === expectedBillAmount)
-  }
-  catch{
-    case error: Exception =>
-      println("Error in calculating bill" +error.getMessage)
   }
   }
 
